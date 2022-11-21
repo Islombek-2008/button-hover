b39462a772282b44b7303455dd167f6d39b8bd9d
Islombek-2008 <playerislombek@gmail.com>
2022-11-21 20:52:29 +0500

    button-hover

---
+++ file/45456/js/scripe.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".auto-type", {
+    strings:["O'zbekiston",
+     "Tojikiston", 
+     "Turkmaniston", 
+     "Qirg'iziston", 
+     "Qozog'iston"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true,
+})
